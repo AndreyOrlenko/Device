c9729c75ee837704de10cc90f38b69afea8b6d94
AndreyOrlenko <andriiorlenkoo@gmail.com>
2018-05-24 23:01:13 +0300

    Замена const на var в gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,30 @@
 'use strict';
-const gulp         = require('gulp');
-const less         = require('gulp-less');//Преобразует less в css
-const concat       = require('gulp-concat'); //Объедияет файлы
-const debug        = require('gulp-debug'); //Выводит информацию о проделанной работе в консоль
-const sourcemaps   = require('gulp-sourcemaps');// карта файла, из чего он состоит. Позволяет видеть в браузере все составляющие файла
-const del          = require('del'); // удаление дерикторий
-const uglify       = require('gulp-uglify'); // Минимизирует js файлы
-const imgMin       = require('gulp-imagemin'); // минификация изображений
-const pngquant     = require('imagemin-pngquant'); // минифицирует png
-const notify       = require('gulp-notify'); // обработчик ошибок
-const merge        = require('merge-stream'); // Сливает несколько потоков в один
-const browserSync  = require('browser-sync'); // обновляет страницу в браузере
-const cssMin       = require('gulp-minify-css'); //  Минимизирует css файлы
-const spritesmith  = require('gulp.spritesmith'); //Создает png спрайты
-const paths        = require('path'); // пути
-const replaceUrl   = require('gulp-css-replace-url'); // Замена url путей в css файлах
-const svgSprite    = require('gulp-svg-sprite'); // создание svg спрайтов
-const combiner     = require('stream-combiner2').obj; // нужен для обработки ошибок в тасках
-const filter       = require('gulp-filter'); // Пропускает через себя только определенные файлы
-const ignore       = require('gulp-ignore'); // Игнорирует определенные файлы
-const rigger       = require('gulp-rigger'); // можно импортировать куски кода при помощи конструкции //= footer.html
-const rename       = require('gulp-rename'); // изменяет имя файла .pipe(rename('vendor.min.js`))
-const cached       = require('gulp-cached'); // пропускает через себя файлы и файлы с одним и тем же именем и содержимым не пропускает
-const autoprefix   = require('gulp-autoprefixer'); // добавляет префиксы
-const newer        = require('gulp-newer');// сравнивает поступающие файлы с файлами в конечной директории
-const If           = require('gulp-if');
+var gulp         = require('gulp');
+var less         = require('gulp-less');//Преобразует less в css
+var concat       = require('gulp-concat'); //Объедияет файлы
+var debug        = require('gulp-debug'); //Выводит информацию о проделанной работе в консоль
+var sourcemaps   = require('gulp-sourcemaps');// карта файла, из чего он состоит. Позволяет видеть в браузере все составляющие файла
+var del          = require('del'); // удаление дерикторий
+var uglify       = require('gulp-uglify'); // Минимизирует js файлы
+var imgMin       = require('gulp-imagemin'); // минификация изображений
+var pngquant     = require('imagemin-pngquant'); // минифицирует png
+var notify       = require('gulp-notify'); // обработчик ошибок
+var merge        = require('merge-stream'); // Сливает несколько потоков в один
+var browserSync  = require('browser-sync'); // обновляет страницу в браузере
+var cssMin       = require('gulp-minify-css'); //  Минимизирует css файлы
+var spritesmith  = require('gulp.spritesmith'); //Создает png спрайты
+var paths        = require('path'); // пути
+var replaceUrl   = require('gulp-css-replace-url'); // Замена url путей в css файлах
+var svgSprite    = require('gulp-svg-sprite'); // создание svg спрайтов
+var combiner     = require('stream-combiner2').obj; // нужен для обработки ошибок в тасках
+var filter       = require('gulp-filter'); // Пропускает через себя только определенные файлы
+var ignore       = require('gulp-ignore'); // Игнорирует определенные файлы
+var rigger       = require('gulp-rigger'); // можно импортировать куски кода при помощи конструкции //= footer.html
+var rename       = require('gulp-rename'); // изменяет имя файла .pipe(rename('vendor.min.js`))
+var cached       = require('gulp-cached'); // пропускает через себя файлы и файлы с одним и тем же именем и содержимым не пропускает
+var autoprefix   = require('gulp-autoprefixer'); // добавляет префиксы
+var newer        = require('gulp-newer');// сравнивает поступающие файлы с файлами в конечной директории
+var If           = require('gulp-if');
 
 /* IF - пропускает файлы через поток в зависимости от тех или иных условий
  может принимать функцию
@@ -42,7 +42,7 @@ const If           = require('gulp-if');
  .pipe(gulp.dest('./dist/'));
  });
  */
-const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
+var isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 /*При таком условии будет генирироваться sourcemap,
  если нужно создать файл без него, в консоли запускаем ( set NODE_ENV=prodaction )
  и запустить gulp less-css
